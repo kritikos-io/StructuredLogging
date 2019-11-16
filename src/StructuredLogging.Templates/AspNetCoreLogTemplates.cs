9e747c02f7590335858446309e7724a49468067f
Alexandros Kritikos <akritikos@outlook.com>
2019-11-16 23:17:24 +0200

    Changes log templates from class based to constant strings

--- src/StructuredLogging.Templates/AspNetCoreLogTemplates.cs
+++ src/StructuredLogging.Templates/AspNetCoreLogTemplates.cs
@@ -1,7 +1,5 @@
 namespace Kritikos.StructuredLogging.Templates
 {
-	using Kritikos.StructuredLogging.Templates.Base;
-
 	/// <summary>
 	/// Logging templates for ASP.NET Core.
 	/// </summary>
@@ -10,44 +8,37 @@ namespace Kritikos.StructuredLogging.Templates
 		/// <summary>
 		/// List of migrations about to be applied to database.
 		/// </summary>
-		public static LogTemplate ApplyingMigrations
-			=> new LogTemplate("Applying migrations to database: {@Migrations}");
+		public const string ApplyingMigrations = "Applying migrations to database: {@Migrations}";
 
 		/// <summary>
 		/// Client requested unavailable entity.
 		/// </summary>
-		public static LogTemplate EntityNotFound
-			=> new LogTemplate("{Entity} {Identifier} not found or not accessible for requesting user");
+		public const string EntityNotFound = "{Entity} {Identifier} not found or not accessible for requesting user";
 
 		/// <summary>
 		/// Entity to be updated has been altered by someone else.
 		/// </summary>
-		public static LogTemplate ConcurrencyError
-			=> new LogTemplate("Concurrency error while updating {@Entity}");
+		public const string ConcurrencyError = "Concurrency error while updating {@Entity}";
 
 		/// <summary>
 		/// Entity to be updated has been altered by someone else, with conflicting properties listed.
 		/// </summary>
-		public static LogTemplate ConcurrencyErrorVerbose
-			=> new LogTemplate("Concurrency error while updating {@Entity}: {@Conflicts}");
+		public const string ConcurrencyErrorVerbose = "Concurrency error while updating {@Entity}: {@Conflict}";
 
 		/// <summary>
 		/// A hard delete is about to occur.
 		/// </summary>
-		public static LogTemplate PermanentDeletion
-			=> new LogTemplate("Permanent deletion of {@Entity} requested by {User}");
+		public const string PermanentDeletion = "Permanent deletion of {@Entity} requested by {User}";
 
 		/// <summary>
 		/// Invalid transition requested.
 		/// </summary>
-		public static LogTemplate InvalidTransition
-			=> new LogTemplate("Invalid transition requested for {@Entity}: {$Status}");
+		public const string InvalidTransition = "Invalid transition requested for {@Entity}: {$Status}";
 
 		/// <summary>
 		/// Invalid transition requested, with supported transitions listed.
 		/// </summary>
-		public static LogTemplate InvalidTransitionVerbose
-			=> new LogTemplate(
-				"Invalid transition requested for {@Entity}: {$Status}. Supported transitions: {@Transitions}");
+		public const string InvalidTransitionVerbose =
+			"Invalid transition requested for {@Entity}: {$Status}. Supported transitions: {@Transitions}";
 	}
 }
